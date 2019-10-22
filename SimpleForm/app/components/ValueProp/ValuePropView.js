e0311dbb71a391ff1876192f64d9b835397990ee
Otto Grajeda <ottograjeda@gmail.com>
2019-10-22 13:45:24 -0700

    Update

--- SimpleForm/app/components/ValueProp/ValuePropView.js
+++ SimpleForm/app/components/ValueProp/ValuePropView.js
@@ -6,7 +6,7 @@ import Newsletter from '../../components/Newsletter';
 const ValueProp = (props) => (
   <section className="vp" style={{alignItems:"center", justifyContent:"center"}} >
         <h1>  I want it !</h1>
-        <h2>Get priority access<br />to exclusive deals !!</h2> <br />
+        <h2>Get access<br />to exclusive deals !!</h2> <br />
         <Newsletter />
         <small>By submitting, you agree to recieve<br/>email periodically and can opt-out at any time.</small><br />
     {/* if needed, overlay div goes here */}
